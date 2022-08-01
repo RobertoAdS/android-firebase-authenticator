ec9ea7b81cab4db3ed3ad598048bcafebac30848
cauehenrique <caue313@gmail.com>
2022-08-01 08:53:59 -0300

    first commit & add: FragmentContainerView stuff

---
+++ app/src/main/java/com/example/authenticator/Congratulations.kt
@@ -0,0 +1,11 @@
+package com.example.authenticator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Congratulations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congratulations)
+    }
+}
